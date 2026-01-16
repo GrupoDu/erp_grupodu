8b72bcb08aa652c86631d6d7bfb685a4d3e09458
João Lucas <lgbr212@gmail.com>
2026-01-16 12:04:53 -0300

    feat(login): Criando middleware de para validar login

---
+++ src/middlewares/validateUserLogin.middleware.ts
@@ -0,0 +1,51 @@
+import type { NextFunction, Request, Response } from "express";
+import bcrypt from "bcrypt";
+import { prisma } from "../../lib/prisma.js";
+
+type loginValues = {
+  email: string;
+  password: string;
+};
+
+export async function validateUser(
+  req: Request,
+  res: Response,
+  next: NextFunction
+): Promise<void> {
+  try {
+    const { email, password }: loginValues = req.body;
+
+    const userTryingToLogin = await prisma.user.findFirst({
+      where: {
+        email: email,
+      },
+    });
+
+    if (!userTryingToLogin) {
+      res.status(401).json({ message: "Usuário não encontrado." });
+      return;
+    }
+
+    const userPasswordDecode = await bcrypt.compare(
+      password,
+      userTryingToLogin.password
+    );
+
+    if (!userPasswordDecode) {
+      res.status(401).json({ message: "Senha ou email incorretos." });
+      return;
+    }
+
+    req.authenticadedUser = userTryingToLogin;
+    next();
+    return;
+  } catch (err) {
+    res.status(500).json({
+      message: "Erro interno do servidor.",
+      error: (err as Error).message,
+    });
+    return;
+  }
+}
+
+export default validateUser;
