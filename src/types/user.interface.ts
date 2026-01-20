8ca1fcbcd409a755352cbd63a9c114a8c49e0f62
João Lucas <lgbr212@gmail.com>
2026-01-20 11:48:19 -0300

    feat(types): Criando tipos novos para user

--- src/types/user.interface.ts
+++ src/types/user.interface.ts
@@ -1,10 +1,17 @@
-export interface IUser {
+interface IUser {
+  user_id: string;
   name: string;
   email: string;
   password: string;
   user_type: string;
 }
 
-export interface IUserResponse extends IUser {
+export interface IUserUpdate extends Partial<IUser> {
   user_id: string;
 }
+
+export interface IUserCreate extends Omit<IUser, "user_id"> {}
+
+export interface IUserResponse extends Omit<IUser, "password"> {}
+
+export interface IUserLogin extends Pick<IUser, "email" | "password"> {}
