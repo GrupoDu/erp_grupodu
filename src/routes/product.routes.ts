fd45d060567a2a73a945ee152c496c27f2d5122b
João Lucas <lgbr212@gmail.com>
2026-01-20 09:02:09 -0300

    refactor(product): Criando uma única instancia do controller e service

--- src/routes/product.routes.ts
+++ src/routes/product.routes.ts
@@ -1,10 +1,14 @@
 import ProductController from "../controllers/product.controller.js";
 import express, { type Request, type Response } from "express";
+import ProductService from "../services/product.service.js";
+import { prisma } from "../../lib/prisma.js";
 
 const router = express.Router();
+const productService = new ProductService(prisma);
+const productController = new ProductController(productService);
 
 router.get("/", (req: Request, res: Response) =>
-  new ProductController().getAllProductsData(req, res)
+  productController.getAllProductsData(req, res),
 );
 
 export default router;
