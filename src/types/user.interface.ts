cdc7e50784932a36cb7ec5e1c93055dec614fa3f
João Lucas <lgbr212@gmail.com>
2026-01-22 14:46:59 -0300

    remove(user): Removendo IUserLogin

--- src/types/user.interface.ts
+++ src/types/user.interface.ts
@@ -10,8 +10,6 @@ export interface IUserUpdate extends Partial<Omit<IUser, "user_id">> {}
 
 export interface IUserCreate extends Omit<IUser, "user_id"> {}
 
-export interface IUserLogin extends Pick<IUser, "email" | "password"> {}
-
 export interface IUserPublic extends Omit<IUser, "password"> {}
 
 export interface IUserWithPassword extends IUser {
