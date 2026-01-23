6179b8222c16d703e9b1efa4a2195dc0a262a65c
João Lucas <lgbr212@gmail.com>
2026-01-23 11:59:09 -0300

    remove: Removendo arquivos desnecessários

--- src/types/mandatoryBodyValues.interface.ts
+++
@@ -1,7 +0,0 @@
-import type { IUser } from "./models.interface.js";
-
-export interface IMandatoryBodyValues extends IUser {
-    description: string;
-    product_type: string;
-    image: string;
-} 
--- src/types/productionTypeGuard.interface.ts
+++
@@ -1,6 +0,0 @@
-import type { register } from "@prisma/client";
-import type { IRegister } from "./models.interface.js";
-
-export function registerExists(register: IRegister[]): register is IRegister[] {
-  return register !== undefined;
-}
