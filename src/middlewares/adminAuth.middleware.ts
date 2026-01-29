4d8dda59834f4c3fa005a141402b4bde80871907
João Lucas <lgbr212@gmail.com>
2026-01-29 17:10:24 -0300

    feat(adminAuthMiddleware): Criando middleware para rotas privadas apenas para admin

---
+++ src/middlewares/adminAuth.middleware.ts
@@ -0,0 +1,64 @@
+import type { Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+import type { IUserPublic } from "../types/user.interface.js";
+
+dotenv.config();
+
+export function adminAuthMiddleware(
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) {
+  try {
+    const token = getToken(req, res);
+
+    if (token === null) throw new Error("Token inválido.");
+
+    if (typeof token !== "string")
+      throw new Error("Formato de token inválido.");
+
+    if (!process.env.JWT_SECRET) throw new Error("SECRET faltando.");
+
+    const payload = jwt.verify(token, process.env.JWT_SECRET) as IUserPublic;
+
+    if (payload.user_type !== "Admin") {
+      return res.status(403).json({ message: "não autorizado." });
+    }
+
+    req.user = payload;
+
+    next();
+  } catch (err) {
+    const error = err as Error;
+
+    if (error.name === "TokenExpiredError") {
+      return res
+        .status(401)
+        .json({ message: "Token expirado. Faça login novamente." });
+    }
+
+    if (error.name === "JsonWebTokenError") {
+      return res.status(401).json({ message: "Token inválido." });
+    }
+
+    console.error("Erro no middleware de autenticação:", error);
+    return res
+      .status(500)
+      .json({ message: "Erro no servidor.", error: error.message });
+  }
+}
+
+function getToken(req: Request, res: Response): string | null | undefined {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader) {
+    throw new Error("TOKEN_NOT_PROVIDED");
+  }
+
+  if (!authHeader.startsWith("Bearer ")) {
+    throw new Error("INVALID_TOKEN_FORMAT");
+  }
+
+  return authHeader.split(" ")[1];
+}
