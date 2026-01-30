2736f24910c422dac33cd45b41b37e1f7e722081
João Lucas <lgbr212@gmail.com>
2026-01-30 14:56:58 -0300

    feat(employee): Adicionando middleware para validar permissão do usuário

--- src/routes/employee.routes.ts
+++ src/routes/employee.routes.ts
@@ -2,21 +2,22 @@ import express, { type Request, type Response } from "express";
 import EmployeeController from "../controllers/employee.controller.js";
 import { prisma } from "../../lib/prisma.js";
 import EmployeeService from "../services/employee.service.js";
+import { adminAuthMiddleware } from "../middlewares/adminAuth.middleware.js";
 
 const router = express.Router();
 const employeeService = new EmployeeService(prisma);
 const employeeController = new EmployeeController(employeeService);
 
-router.get("/", (req: Request, res: Response) => {
+router.get("/", adminAuthMiddleware, (req: Request, res: Response) => {
   employeeController.getAllEmployeesData(req, res);
 });
-router.post("/", (req: Request, res: Response) => {
+router.post("/", adminAuthMiddleware, (req: Request, res: Response) => {
   employeeController.createNewEmployee(req, res);
 });
-router.delete("/:uuid", (req: Request, res: Response) => {
+router.delete("/:uuid", adminAuthMiddleware, (req: Request, res: Response) => {
   employeeController.removeEmployeeData(req, res);
 });
-router.put("/:uuid", (req: Request, res: Response) => {
+router.put("/:uuid", adminAuthMiddleware, (req: Request, res: Response) => {
   employeeController.updateEmployeeData(req, res);
 });
 
