d93d7d817f967fbd475b18801fbf86d7dde89ccd
João Lucas <lgbr212@gmail.com>
2026-02-05 09:49:17 -0300

    feat(dataAnalysis): Omitindo campo de registro pendente na interface IEmployeeProductionAnalysis

--- src/types/dataAnalysis.interface.ts
+++ src/types/dataAnalysis.interface.ts
@@ -1,6 +1,7 @@
 export interface IProductionAnalysis {
   deliveredRegisterQuantity: number;
   notDeliveredRegisterQuantity: number;
+  pendingRegisterQuantity: number;
   actualMonth: Date;
   nextMonth: Date;
 }
@@ -9,6 +10,9 @@ export interface IProductProductionAnalysis extends IProductionAnalysis {
   productName: string;
 }
 
-export interface IEmployeeProductionAnalysis extends IProductionAnalysis {
+export interface IEmployeeProductionAnalysis extends Omit<
+  IProductionAnalysis,
+  "pendingRegisterQuantity"
+> {
   employeeName: string;
 }
