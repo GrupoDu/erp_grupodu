8d503cb8118d5c9b9ddacb88dacc269a4ee274bf
João Lucas <lgbr212@gmail.com>
2026-02-13 10:39:30 -0300

    refactor(productionOrder): Alterando middleware de GETs e PUT

--- src/routes/productionOrder.routes.ts
+++ src/routes/productionOrder.routes.ts
@@ -4,6 +4,7 @@ import type { Request, Response } from "express";
 import ProductionOrderService from "../services/productionOrder.service.js";
 import { prisma } from "../../lib/prisma.js";
 import { adminAuthMiddleware } from "../middlewares/adminAuth.middleware.js";
+import authMiddleware from "../middlewares/auth.middleware.js";
 
 const router = express.Router();
 const productionOrderService = new ProductionOrderService(prisma);
@@ -11,19 +12,19 @@ const productionOrderController = new ProductionOrderController(
   productionOrderService,
 );
 
-router.get("/", (req: Request, res: Response) =>
+router.get("/", authMiddleware, (req: Request, res: Response) =>
   productionOrderController.getAllProductionRegisters(req, res),
 );
 router.post("/", adminAuthMiddleware, (req: Request, res: Response) =>
   productionOrderController.createNewProductionOrder(req, res),
 );
-router.get("/:uuid", adminAuthMiddleware, (req: Request, res: Response) =>
+router.get("/:uuid", authMiddleware, (req: Request, res: Response) =>
   productionOrderController.getProductionOrderById(req, res),
 );
 router.delete("/:uuid", adminAuthMiddleware, (req: Request, res: Response) =>
   productionOrderController.removeTask(req, res),
 );
-router.put("/:uuid", (req: Request, res: Response) =>
+router.put("/:uuid", authMiddleware, (req: Request, res: Response) =>
   productionOrderController.updateProductionOrder(req, res),
 );
 
