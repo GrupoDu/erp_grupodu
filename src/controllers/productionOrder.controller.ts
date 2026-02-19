dd7ecf56ed3857f65cdc5391a1e8d429dab0a257
João Lucas <lgbr212@gmail.com>
2026-02-19 11:02:05 -0300

    feat(productionOrder): Criando metódo para entrega

--- src/controllers/productionOrder.controller.ts
+++ src/controllers/productionOrder.controller.ts
@@ -4,7 +4,7 @@ import { responseMessages } from "../constants/messages.constants.ts";
 
 class ProductionOrderController {
   private productionOrderService: ProductionOrderService;
-  
+
   constructor(productionOrderService: ProductionOrderService) {
     this.productionOrderService = productionOrderService;
   }
@@ -106,6 +106,31 @@ class ProductionOrderController {
       return res.status(200).json({
         message: "Registro atualizado com sucesso.",
         update: updatedProductionOrder,
+      });
+    } catch (err) {
+      return res.status(500).json({
+        message: responseMessages.catchErrorMessage,
+        error: (err as Error).message,
+      });
+    }
+  }
+
+  async deliverProductionOrder(req: Request, res: Response): Promise<Response> {
+    try {
+      const { production_order_id } = req.params;
+      const { delivered_product_quantity, requested_product_quantity } =
+        req.body;
+
+      const deliveredProductionOrder =
+        await this.productionOrderService.deliverProductionOrder(
+          production_order_id as string,
+          delivered_product_quantity as number,
+          requested_product_quantity as number,
+        );
+
+      return res.status(200).json({
+        message: "Ordem de produção entregue com sucesso.",
+        update: deliveredProductionOrder,
       });
     } catch (err) {
       return res.status(500).json({
