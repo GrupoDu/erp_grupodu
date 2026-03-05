3fcaa39a186492deba640e9d83fa5addc054f2c8
João Lucas <lgbr212@gmail.com>
2026-03-05 09:58:14 -0300

    feat(goalsAnalysis): Adicionando middlewares na rota GET

--- src/routes/goalsAnalysis.routes.ts
+++ src/routes/goalsAnalysis.routes.ts
@@ -3,6 +3,8 @@ import type { Request, Response, Router } from "express";
 import GoalsAnalysisController from "../controllers/goalsAnalysis.controller.js";
 import GoalsAnalysisService from "../services/goalsAnalysis.service.js";
 import { prisma } from "../../lib/prisma.js";
+import { getTokenMiddleware } from "../middlewares/getToken.middleware.js";
+import { adminAuthMiddleware } from "../middlewares/adminAuth.middleware.js";
 
 const router: Router = express.Router();
 const goalsAnalysisService = new GoalsAnalysisService(prisma);
@@ -10,8 +12,12 @@ const goalsAnalysisController = new GoalsAnalysisController(
   goalsAnalysisService,
 );
 
-router.get("/", (req: Request, res: Response) =>
-  goalsAnalysisController.getGoalsAnalysis(req, res),
+router.get(
+  "/",
+  getTokenMiddleware,
+  adminAuthMiddleware,
+  (req: Request, res: Response) =>
+    goalsAnalysisController.getGoalsAnalysis(req, res),
 );
 
 export default router;
