8f6b26effc767b42c9a307220b584541682f5800
João Lucas <lgbr212@gmail.com>
2026-03-05 11:48:39 -0300

    feat(inoutStock): Criando rotas para inoutStock

---
+++ src/routes/inoutStock.routes.ts
@@ -0,0 +1,22 @@
+import express from "express";
+import type { Request, Response, Router } from "express";
+import InOutStockController from "../controllers/inoutStock.controller.ts";
+import InOutStockService from "../services/inoutStock.service.ts";
+import { prisma } from "../../lib/prisma.ts";
+import { getTokenMiddleware } from "../middlewares/getToken.middleware.ts";
+
+const router: Router = express.Router();
+const inoutStockService = new InOutStockService(prisma);
+const inoutStockController = new InOutStockController(inoutStockService);
+
+router.get("/", getTokenMiddleware, (req: Request, res: Response) =>
+  inoutStockController.getInOutStockAnalysis(req, res),
+);
+router.post("/incrementInStock", (req: Request, res: Response) =>
+  inoutStockController.incrementMonthlyInStockQuantity(req, res),
+);
+router.post("/incrementOutStock", (req: Request, res: Response) =>
+  inoutStockController.incrementMonthlyOutStockQuantity(req, res),
+);
+
+export default router;
