2a96b22a945170d3a845cfffb1a60969d3852caf
jukalbf <lgbr212@gmail.com>
2026-03-25 13:29:52 -0300

    refactor(anualAnalysis): Refatorando controller
    Refatoração do response do controller, para melhor legibilidade e manutenabilidade

--- src/controllers/anualAnalysis.controller.ts
+++ src/controllers/anualAnalysis.controller.ts
@@ -1,7 +1,8 @@
 import type AnualAnalysisService from "../services/anualAnalysis.service.js";
 import type { Request, Response } from "express";
 import type { IAnualAnalysis } from "../types/anualAnalysis.interface.js";
-import { responseMessages } from "../constants/messages.constants.js";
+import errorResponseWith from "../utils/errorResponseWith.js";
+import successResponseWith from "../utils/successResponseWith.js";
 
 class AnualAnalysisController {
   private anualAnalysisService: AnualAnalysisService;
@@ -15,16 +16,17 @@ class AnualAnalysisController {
       const anualAnalysisData: IAnualAnalysis[] =
         await this.anualAnalysisService.getMontlyAnalysis();
 
-      if (!anualAnalysisData) {
-        throw new Error("Nenhum registro nos últimos meses");
-      }
-
-      return res.status(200).json(anualAnalysisData);
+      return res
+        .status(200)
+        .json(
+          successResponseWith(
+            anualAnalysisData,
+            "Dados encontrados com sucesso.",
+          ),
+        );
     } catch (err) {
-      return res.status(500).json({
-        message: responseMessages.catchErrorMessage,
-        error: (err as Error).message,
-      });
+      const error = err as Error;
+      return res.status(500).json(errorResponseWith(error.message, 500));
     }
   }
 
@@ -33,12 +35,17 @@ class AnualAnalysisController {
       const anualAnalysisUpdateResponse: string =
         await this.anualAnalysisService.updateDeliveredMontlyAnalysis();
 
-      return res.status(200).json({ message: anualAnalysisUpdateResponse });
+      return res
+        .status(200)
+        .json(
+          successResponseWith(
+            anualAnalysisUpdateResponse,
+            "Análise anual atualizada com sucesso.",
+          ),
+        );
     } catch (err) {
-      return res.status(500).json({
-        message: responseMessages.catchErrorMessage,
-        error: (err as Error).message,
-      });
+      const error = err as Error;
+      return res.status(500).json(errorResponseWith(error.message, 500));
     }
   }
 }
