7fee65c7fd713cabb9130a1c0921ce10e83d44b7
jukalbf <lgbr212@gmail.com>
2026-03-30 14:19:36 -0300

    refactor(employee): Params e chave
    Alterando nome de params de 'uuid' -> 'employee_uuid' e removendo chaves

--- src/routes/employee.routes.ts
+++ src/routes/employee.routes.ts
@@ -1,62 +1,56 @@
-import express, { Router, type Request, type Response } from "express";
+import { Router, type Request, type Response } from "express";
 import EmployeeController from "../controllers/employee.controller.js";
 import { prisma } from "../../lib/prisma.js";
 import EmployeeService from "../services/employee.service.js";
 import { adminAuthMiddleware } from "../middlewares/adminAuth.middleware.js";
 import { getTokenMiddleware } from "../middlewares/getToken.middleware.js";
 
-const router: Router = express.Router();
+const router: Router = Router();
 const employeeService = new EmployeeService(prisma);
 const employeeController = new EmployeeController(employeeService);
 
-router.get("/", getTokenMiddleware, (req: Request, res: Response) => {
-  employeeController.getAllEmployeesData(req, res);
+router.get("/", getTokenMiddleware, async (req: Request, res: Response) => {
+  await employeeController.getAllEmployeesData(req, res);
 });
 router.post(
   "/",
   getTokenMiddleware,
   adminAuthMiddleware,
-  (req: Request, res: Response) => {
-    employeeController.createNewEmployee(req, res);
-  },
+  (req: Request, res: Response) =>
+    employeeController.createNewEmployee(req, res),
 );
 router.get(
-  "/:uuid",
+  "/:employee_uuid",
   getTokenMiddleware,
   adminAuthMiddleware,
-  (req: Request, res: Response) => {
-    employeeController.getEmployeeData(req, res);
-  },
+  (req: Request, res: Response) =>
+    employeeController.getEmployeeDataById(req, res),
 );
 router.delete(
-  "/:uuid",
+  "/:employee_uuid",
   getTokenMiddleware,
   adminAuthMiddleware,
-  (req: Request, res: Response) => {
-    employeeController.removeEmployeeData(req, res);
-  },
+  (req: Request, res: Response) =>
+    employeeController.removeEmployeeData(req, res),
 );
 router.put(
-  "/:uuid",
+  "/:employee_uuid",
   getTokenMiddleware,
   adminAuthMiddleware,
-  (req: Request, res: Response) => {
-    employeeController.updateEmployeeData(req, res);
-  },
+  (req: Request, res: Response) =>
+    employeeController.updateEmployeeData(req, res),
 );
 router.put(
-  "/activity/:uuid",
+  "/activity/:employee_uuid",
   getTokenMiddleware,
-  (req: Request, res: Response) => {
-    employeeController.incrementEmployeeActivityQuantity(req, res);
-  },
+  (req: Request, res: Response) =>
+    employeeController.incrementEmployeeActivityQuantity(req, res),
 );
 router.put(
-  "/producedQuantity/:uuid",
+  "/producedQuantity/:employee_uuid",
   getTokenMiddleware,
-  (req: Request, res: Response) => {
-    employeeController.incrementEmployeeProductsProducedQuantity(req, res);
-  },
+  (req: Request, res: Response) =>
+    employeeController.incrementEmployeeProductsProducedQuantity(req, res),
 );
 
 export default router;
