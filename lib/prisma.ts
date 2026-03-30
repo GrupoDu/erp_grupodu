92d8d3c61e1fad6237ba5c67db1439a02cfdbe1b
jukalbf <lgbr212@gmail.com>
2026-03-30 13:54:07 -0300

    feat(prisma): Criando interface PrismaTransctionClient

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -7,3 +7,8 @@ const connectionString = process.env.DATABASE_URL;
 
 const adapter = new PrismaPg({ connectionString });
 export const prisma = new PrismaClient({ adapter });
+
+export interface PrismaTransactionClient extends Omit<
+  PrismaClient,
+  "$connect" | "$disconnect" | "$on" | "$use" | "$extends"
+> {}
