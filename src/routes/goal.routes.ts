981606a86e47f6f182ce0903b732cb5a67f54802
jukalbf <lgbr212@gmail.com>
2026-03-30 14:20:12 -0300

    refactor(goal): Params e chave
    Alterando nome de params de 'uuid' -> 'goal_uuid' e removendo chaves

--- src/routes/goal.routes.ts
+++ src/routes/goal.routes.ts
@@ -1,40 +1,34 @@
-import express, { Router, type Request, type Response } from "express";
+import { Router, type Request, type Response } from "express";
 import GoalController from "../controllers/goal.controller.js";
 import GoalService from "../services/goal.service.js";
 import { prisma } from "../../lib/prisma.js";
 import { adminAuthMiddleware } from "../middlewares/adminAuth.middleware.js";
 import { getTokenMiddleware } from "../middlewares/getToken.middleware.js";
 
-const router: Router = express.Router();
+const router: Router = Router();
 const goalService = new GoalService(prisma);
 const goalController = new GoalController(goalService);
 
-router.get("/", getTokenMiddleware, (req: Request, res: Response) => {
-  goalController.getAllGoalsData(req, res);
-});
+router.get("/", getTokenMiddleware, (req: Request, res: Response) =>
+  goalController.getAllGoalsData(req, res),
+);
 router.post(
   "/",
   getTokenMiddleware,
   adminAuthMiddleware,
-  (req: Request, res: Response) => {
-    goalController.createNewGoal(req, res);
-  },
+  (req: Request, res: Response) => goalController.createNewGoal(req, res),
 );
 router.delete(
-  "/:uuid",
+  "/:goal_uuid",
   getTokenMiddleware,
   adminAuthMiddleware,
-  (req: Request, res: Response) => {
-    goalController.removeGoalData(req, res);
-  },
+  (req: Request, res: Response) => goalController.removeGoalData(req, res),
 );
 router.put(
-  "/:uuid",
+  "/:goal_uuid",
   getTokenMiddleware,
   adminAuthMiddleware,
-  (req: Request, res: Response) => {
-    goalController.updateGoalData(req, res);
-  },
+  (req: Request, res: Response) => goalController.updateGoalData(req, res),
 );
 
 export default router;
