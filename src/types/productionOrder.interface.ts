6f8bfcafba25e21921cf55b8c2c6d842de3fbaf8
jukalbf <lgbr212@gmail.com>
2026-03-30 13:43:01 -0300

    feat(productionOrder): Atualizando omit e teste
    Criando 'type' para definir todos os campos que devem ser omitidos , passando para 'IProductionOrderCreate' e criando interface para testes

--- src/types/productionOrder.interface.ts
+++ src/types/productionOrder.interface.ts
@@ -18,12 +18,36 @@ export interface IProductionOrder {
   product_uuid: string;
   client_uuid: string;
   delivered_product_quantity?: Decimal;
+  stock_validation?: boolean;
 }
+
+type createOmitFields =
+  | "production_order_id"
+  | "created_at"
+  | "delivered_product_quantity"
+  | "stock_validation"
+  | "delivered_at"
+  | "employee_uuid"
+  | "finishing_assistant"
+  | "paint_assistant"
+  | "fold_assistant"
+  | "cut_assistant";
 
 export interface IProductionOrderCreate extends Omit<
   IProductionOrder,
-  "production_order_id" | "created_at" | "delivered_product_quantity"
-> {}
+  createOmitFields | "product_uuid" | "client_uuid"
+> {
+  users: {
+    connect: {
+      user_id: string;
+    };
+  };
+  products: {
+    connect: {
+      uuid: string;
+    };
+  };
+}
 
 export interface IProductionOrderUpdate extends Partial<
   Omit<IProductionOrder, "production_order_id">
@@ -33,3 +57,11 @@ export interface IProductionOrderDeliver extends Pick<
   IProductionOrder,
   "production_order_id" & "delivered_product_quantity" & "product_quantity"
 > {}
+
+export interface IProductionOrderTestType extends Omit<
+  IProductionOrder,
+  "delivered_product_quantity" | "product_quantity"
+> {
+  product_quantity: number;
+  delivered_product_quantity: number;
+}
