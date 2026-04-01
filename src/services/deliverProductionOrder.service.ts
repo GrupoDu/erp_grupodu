29e76f116197b8ee79676a93bda7fdb26c3fe257
jukalbf <lgbr212@gmail.com>
2026-04-01 08:57:03 -0300

    feat(deliverProductionOrder): Validação de entrega
    Verificando se todos os assistentes estão definidos antes de entregar ordem de produção

--- src/services/deliverProductionOrder.service.ts
+++ src/services/deliverProductionOrder.service.ts
@@ -1,6 +1,8 @@
 import type { PrismaClient } from "../../generated/prisma/client.js";
 import type { IProductionOrder } from "../types/productionOrder.interface.js";
 import AssistantsPoRegistersService from "./assistantsPoRegisters.service.js";
+import type { PrismaTransactionClient } from "../../lib/prisma.js";
+import debbugLogger from "../utils/debugLogger.js";
 
 /**
  * Service responsável por entregar uma ordem de produção.
@@ -24,35 +26,45 @@ class DeliverProductionOrderService {
     delivered_product_quantity: number,
     requested_product_quantity: number,
   ): Promise<IProductionOrder> {
-    this.verifyDeliveredProductQuantity(
-      delivered_product_quantity,
-      requested_product_quantity,
-    );
-
-    const areAllAssistantsDone =
-      await this.assistantsPoRegistersService.isEveryAssistantsPORegistersDone(
+    return this._prisma.$transaction(async (tx) => {
+      const assistantsNotDefined = !(await this.hasAllAssistantsFilled(
         production_order_id,
+        tx,
+      ));
+
+      if (assistantsNotDefined)
+        throw new Error("Verifique se todos os assistentes foram definidos.");
+
+      this.verifyDeliveredProductQuantity(
+        delivered_product_quantity,
+        requested_product_quantity,
       );
 
-    if (!areAllAssistantsDone) {
-      throw new Error(
-        "Não é possível entregar a ordem de produção. Existem assistentes que ainda não concluíram suas tarefas.",
-      );
-    }
+      const areAllAssistantsDone =
+        await this.assistantsPoRegistersService.isEveryAssistantsPORegistersDone(
+          production_order_id,
+        );
 
-    const deliveredProductOrder: IProductionOrder =
-      await this._prisma.production_order.update({
-        data: {
-          delivered_at: new Date(),
-          delivered_product_quantity: delivered_product_quantity,
-          production_order_status: "Entregue",
-        },
-        where: {
-          production_order_id: production_order_id,
-        },
-      });
+      if (!areAllAssistantsDone) {
+        throw new Error(
+          "Não é possível entregar a ordem de produção. Existem assistentes que ainda não concluíram suas tarefas.",
+        );
+      }
 
-    return deliveredProductOrder;
+      const deliveredProductOrder: IProductionOrder =
+        await this._prisma.production_order.update({
+          data: {
+            delivered_at: new Date(),
+            delivered_product_quantity: delivered_product_quantity,
+            production_order_status: "Entregue",
+          },
+          where: {
+            production_order_id: production_order_id,
+          },
+        });
+
+      return deliveredProductOrder;
+    });
   }
 
   private verifyDeliveredProductQuantity(
@@ -64,6 +76,24 @@ class DeliverProductionOrderService {
         "Quantidade entregue maior que a quantidade requisitada.",
       );
     }
+  }
+
+  private async hasAllAssistantsFilled(
+    production_order_id: string,
+    tx: PrismaTransactionClient,
+  ): Promise<boolean> {
+    const assistantsCount = await tx.assistants_po_register.count({
+      where: {
+        production_order_uuid: production_order_id,
+      },
+    });
+
+    debbugLogger([
+      "Passando por verificação de assistentes...",
+      `Validação passou como: ${assistantsCount}`,
+    ]);
+
+    return assistantsCount === 4;
   }
 }
 
